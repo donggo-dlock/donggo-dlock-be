a5e2a0fa157c01514810a5c2d72f5c9b210daf53
akfrdma0125 <nogain1215@gmail.com>
2024-01-19 13:11:32 +0900

    refactor(#6): ResourceNotFoundException 공통 처리

--- src/main/java/com/example/base/common/exception/ResourceNotFoundException.java
+++ src/main/java/com/example/base/common/exception/ResourceNotFoundException.java
@@ -1,6 +1,5 @@
-package com.example.base.user.domain.exception;
+package com.example.base.common.exception;
 
-import com.example.base.common.exception.BusinessException;
 import org.springframework.http.HttpStatus;
 
 import static com.example.base.common.domain.BusinessCode.NOT_FOUND;
