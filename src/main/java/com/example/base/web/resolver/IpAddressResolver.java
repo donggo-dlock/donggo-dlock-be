71aade7c05d5e092bfa0518d99fa0f7ec307f3fb
akfrdma0125 <nogain1215@gmail.com>
2024-01-23 14:55:40 +0900

    feat(#10): ip주소 없는 경우 예외 처리

--- src/main/java/com/example/base/web/resolver/IpAddressResolver.java
+++ src/main/java/com/example/base/web/resolver/IpAddressResolver.java
@@ -1,7 +1,9 @@
 package com.example.base.web.resolver;
 
+import com.example.base.common.exception.ResourceNotFoundException;
 import com.example.base.web.annotation.IpAddress;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
@@ -19,16 +21,19 @@ public class IpAddressResolver implements HandlerMethodArgumentResolver {
     }
 
     @Override
-    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+    public Object resolveArgument(@NotNull MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory)  {
         HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
 
-        String remoteAddr = "";
+        String remoteAddr;
 
         if (request.getHeader("X-Forwarded-For") != null) {
             remoteAddr = request.getHeader("X-Forwarded-For").split(",")[0];
         } else {
             remoteAddr = request.getRemoteAddr();
         }
+
+        if (remoteAddr.isEmpty())
+            throw new ResourceNotFoundException("IpAddress", remoteAddr);
 
         return remoteAddr;
     }
