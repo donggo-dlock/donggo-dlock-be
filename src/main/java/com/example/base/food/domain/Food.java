2e7c29576c738f364397b9c417a848ebe31d6479
akfrdma0125 <nogain1215@gmail.com>
2024-01-24 10:18:51 +0900

    feat(#10): Food 생성 메서드

--- src/main/java/com/example/base/food/domain/Food.java
+++ src/main/java/com/example/base/food/domain/Food.java
@@ -1,6 +1,9 @@
 package com.example.base.food.domain;
 
 import com.example.base.commentable.domain.Commentable;
+import com.example.base.common.service.port.ClockHolder;
+import com.example.base.food.domain.dto.FoodCreate;
+import com.example.base.reportable.domain.ActiveStatus;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,4 +12,20 @@ import lombok.Setter;
 public class Food extends Commentable {
     private int daysBeforeTest;
     private String mainIngredient;
+
+    public static Food from(FoodCreate foodCreate, ClockHolder clockHolder) {
+        Food food = new Food();
+        food.setName(foodCreate.name());
+        food.setContent(foodCreate.content());
+        food.setUserInformation(foodCreate.userInformation());
+        food.setPassword(foodCreate.password());
+        food.setStatus(ActiveStatus.ACTIVE);
+        food.setDaysBeforeTest(foodCreate.daysBeforeTest());
+        food.setMainIngredient(foodCreate.mainIngredient());
+        food.setCreatedAt(clockHolder.millis());
+        food.setLikes(0);
+        food.setDislikes(0);
+        food.setViews(0);
+        return food;
+    }
 }
