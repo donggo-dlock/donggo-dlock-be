9542560294d55fe247cdae563e57a778f59cea2e
akfrdma0125 <nogain1215@gmail.com>
2024-01-25 09:37:30 +0900

    docs(#10): food list 조회 설명 추가

--- src/main/java/com/example/base/web/dto/PageCreate.java
+++ src/main/java/com/example/base/web/dto/PageCreate.java
@@ -1,6 +1,7 @@
 package com.example.base.web.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import lombok.Builder;
@@ -15,9 +16,11 @@ import lombok.Setter;
 @NoArgsConstructor
 public class PageCreate {
 
+    @Schema(description = "페이지 번호", example = "0", defaultValue = "0")
     @Min(value = 0, message = "페이지는 0보다 커야 합니다.")
     private int page;
 
+    @Schema(description = "페이지 크기", example = "10", defaultValue = "10")
     @Min(value = 0, message = "페이지 크기는 0보다 커야 합니다.")
     @Max(value = 1000, message = "페이지 크기는 1000보다 작아야 합니다.")
     private int size;
