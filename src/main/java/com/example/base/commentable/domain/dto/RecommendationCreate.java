1ac35afd1e4d7d3753ec6b2c6a6e9fd6f67bd9f2
akfrdma0125 <nogain1215@gmail.com>
2024-01-26 13:20:57 +0900

    docs(#13): 추천 api 설명 추가

--- src/main/java/com/example/base/commentable/domain/dto/RecommendationCreate.java
+++ src/main/java/com/example/base/commentable/domain/dto/RecommendationCreate.java
@@ -7,16 +7,18 @@ import lombok.Builder;
 @Builder
 @Schema(description = "추천/비추천 생성 요청")
 public record RecommendationCreate(
-    @Schema(description = "추천/비추천 여부", example = "true")
+    @Schema(description = "추천/비추천 여부", example = "true", allowableValues = {"true", "false"})
     @NotNull
     Boolean RecommendationFlag,
 
     @Schema(description = "추천/비추천 카테고리", allowableValues = {"FOOD","REVIEW"})
     @Pattern(regexp ="^(FOOD|REVIEW)$", message = "Category는 FOOD, REVIEW 중 하나여야 합니다.")
+    @NotBlank
     String Category,
 
     @Schema(description = "추천/비추천 대상 ID")
     @Min(value = 1, message = "id는 1 이상이어야 합니다.")
+    @NotNull
     Long id
 ) {
 }
