a5298f0b2c4534fe573e67715b0dafec2380d9df
akfrdma0125 <nogain1215@gmail.com>
2024-01-26 13:11:57 +0900

    feat(#13): 리뷰 도메인 추천/비추천 증가 구현

--- src/main/java/com/example/base/review/service/ReviewServiceImpl.java
+++ src/main/java/com/example/base/review/service/ReviewServiceImpl.java
@@ -1,7 +1,10 @@
 package com.example.base.review.service;
 
 
+import com.example.base.common.service.port.ClockHolder;
 import com.example.base.review.controller.port.ReviewService;
+import com.example.base.review.domain.Review;
+import com.example.base.review.service.port.ReviewRepository;
 import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,4 +13,12 @@ import org.springframework.stereotype.Service;
 @Builder
 @RequiredArgsConstructor
 public class ReviewServiceImpl implements ReviewService {
+    private final ReviewRepository reviewRepository;
+    private final ClockHolder clockHolder;
+    @Override
+    public void updateRecommendations(Long id, Boolean recommendationFlag) {
+        Review review = reviewRepository.get(id);
+        review.updateRecommendations(recommendationFlag);
+        reviewRepository.save(review);
+    }
 }
--- src/main/java/com/example/base/review/service/port/ReviewRepository.java
+++ src/main/java/com/example/base/review/service/port/ReviewRepository.java
@@ -1,5 +1,14 @@
 package com.example.base.review.service.port;
 
 
+import com.example.base.review.domain.Review;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
 public interface ReviewRepository {
+     @Transactional(propagation = Propagation.MANDATORY)
+     void save(Review review);
+
+     @Transactional(propagation = Propagation.MANDATORY, readOnly = true)
+     Review get(Long id);
 }
