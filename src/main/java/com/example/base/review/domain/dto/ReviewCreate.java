bd6cf06ccb828122812711f6ec75e81208ef1bc3
akfrdma0125 <nogain1215@gmail.com>
2024-01-29 17:18:03 +0900

    feat(#15): 리뷰 생성 기능 구현

---
+++ src/main/java/com/example/base/review/domain/dto/ReviewCreate.java
@@ -0,0 +1,31 @@
+package com.example.base.review.domain.dto;
+
+
+import com.example.base.common.service.port.PasswordHolder;
+import com.example.base.review.controller.request.ReviewCreateRequest;
+import lombok.Builder;
+
+@Builder
+public record ReviewCreate(
+    String name,
+    String userInformation,
+    String password,
+    String content,
+    int age,
+    char gender,
+    boolean sleepFlag,
+    String result
+) {
+    public static ReviewCreate from(ReviewCreateRequest reviewCreateRequest, String userInformation, PasswordHolder passwordHolder) {
+        return ReviewCreate.builder()
+            .name(reviewCreateRequest.name())
+            .userInformation(userInformation)
+            .password(passwordHolder.encrypt(reviewCreateRequest.password()))
+            .content(reviewCreateRequest.content())
+            .age(reviewCreateRequest.age())
+            .gender(reviewCreateRequest.gender())
+            .sleepFlag(reviewCreateRequest.sleepFlag())
+            .result(reviewCreateRequest.result())
+            .build();
+    }
+}
