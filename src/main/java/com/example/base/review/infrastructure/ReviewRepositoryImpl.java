cf912bd1ff21e58bd44c1c6b27294c8d775d50cd
akfrdma0125 <nogain1215@gmail.com>
2024-01-29 17:20:04 +0900

    feat(#15): 리뷰 목록 조회 기능 구현

--- src/main/java/com/example/base/review/infrastructure/ReviewRepositoryImpl.java
+++ src/main/java/com/example/base/review/infrastructure/ReviewRepositoryImpl.java
@@ -2,11 +2,20 @@ package com.example.base.review.infrastructure;
 
 import com.example.base.common.exception.ResourceNotFoundException;
 import com.example.base.common.infrastructure.repository.BaseRepository;
+import com.example.base.reportable.domain.ActiveStatus;
 import com.example.base.review.domain.Review;
+import com.example.base.review.domain.dto.ReviewSearch;
 import com.example.base.review.service.port.ReviewRepository;
+import com.example.base.web.dto.PageCreate;
+import com.example.base.web.dto.PageResponse;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.stereotype.Repository;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Repository
@@ -34,5 +43,82 @@ public class ReviewRepositoryImpl extends BaseRepository<ReviewEntity, Long> imp
                 .orElseThrow(() -> new ResourceNotFoundException(REVIEW_MESSAGE, id));
 
         return reviewEntity.toModel();
+    }
+
+    @Override
+    public void delete(Review review) {
+        delete(ReviewEntity.from(review));
+    }
+
+    @Override
+    public PageResponse<Review> getPage(PageCreate pageCreate, ReviewSearch reviewSearch) {
+        List<Review> reviews = getPaginationContent(
+                selectFrom(qReview)
+                        .where(qReview.status.eq(ActiveStatus.ACTIVE),
+                                eqKeyword(reviewSearch.keyword()),
+                                eqAge(reviewSearch.age()),
+                                eqGender(reviewSearch.gender()),
+                                eqSleepFlag(reviewSearch.sleepFlag()),
+                                eqResult(reviewSearch.result()))
+                        .orderBy(getOrderSpecifierList(reviewSearch.sortBy()))
+                , pageCreate).stream().map(ReviewEntity::toModel).toList();
+        Long totalCount = getTotalCount(
+                selectFrom(qReview)
+                        .where(qReview.status.eq(ActiveStatus.ACTIVE),
+                                eqKeyword(reviewSearch.keyword()),
+                                eqAge(reviewSearch.age()),
+                                eqGender(reviewSearch.gender()),
+                                eqSleepFlag(reviewSearch.sleepFlag()),
+                                eqResult(reviewSearch.result()))
+        );
+        return PageResponse.of(reviews, pageCreate, totalCount);
+    }
+
+    private BooleanExpression eqKeyword(final String keyword) {
+        return keyword == null ? null : qReview.name.contains(keyword);
+    }
+
+    private BooleanExpression eqAge(final String age) {
+        if (Objects.isNull(age))
+            return null;
+        int[] ageConditions = Arrays.stream(age.split("#")).mapToInt(Integer::parseInt).toArray();
+        BooleanExpression ageCondition = qReview.age.eq(ageConditions[0]);
+        for (int i = 1; i < ageConditions.length; i++) {
+            ageCondition = ageCondition.or(qReview.age.eq(ageConditions[i]));
+        }
+        return ageCondition;
+    }
+
+    private BooleanExpression eqGender(final char gender) {
+        if (gender == 'M' || gender == 'F')
+            return qReview.gender.eq(gender);
+        if (gender == '\u0000')
+            return null;
+        throw new IllegalArgumentException("gender는 M 또는 F로 설정할 수 있습니다.");
+    }
+
+    private BooleanExpression eqSleepFlag(final Boolean sleepFlag) {
+        if (Objects.isNull(sleepFlag))
+            return null;
+        return qReview.sleepFlag.eq(sleepFlag);
+    }
+
+    private BooleanExpression eqResult(final String result) {
+        if (Objects.isNull(result))
+            return null;
+        String[] resultConditions = result.split("#");
+        BooleanExpression resultCondition = qReview.result.contains(resultConditions[0]);
+        for (int i = 1; i < resultConditions.length; i++) {
+            resultCondition = resultCondition.or(qReview.result.contains(resultConditions[i]));
+        }
+        return resultCondition;
+    }
+
+    private OrderSpecifier<?> getOrderSpecifierList(final String sortBy) {
+        return switch (sortBy) {
+            case "VIEW" -> qReview.views.desc();
+            case "LIKE" -> qReview.likes.desc();
+            default -> qReview.id.desc();
+        };
     }
 }
--- src/main/java/com/example/base/review/service/ReviewServiceImpl.java
+++ src/main/java/com/example/base/review/service/ReviewServiceImpl.java
@@ -1,24 +1,66 @@
 package com.example.base.review.service;
 
 
+import com.example.base.commentable.domain.dto.CommentableDelete;
 import com.example.base.common.service.port.ClockHolder;
+import com.example.base.common.service.port.PasswordHolder;
 import com.example.base.review.controller.port.ReviewService;
+import com.example.base.review.controller.request.ReviewCreateRequest;
+import com.example.base.review.controller.response.ReviewInfoResponse;
+import com.example.base.review.controller.response.ReviewResponse;
 import com.example.base.review.domain.Review;
+import com.example.base.review.domain.dto.ReviewCreate;
+import com.example.base.review.domain.dto.ReviewSearch;
 import com.example.base.review.service.port.ReviewRepository;
+import com.example.base.web.dto.PageCreate;
+import com.example.base.web.dto.PageResponse;
 import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Builder
 @RequiredArgsConstructor
+@Transactional(propagation = Propagation.REQUIRES_NEW)
 public class ReviewServiceImpl implements ReviewService {
     private final ReviewRepository reviewRepository;
+    private final PasswordHolder passwordHolder;
     private final ClockHolder clockHolder;
+
+    @Override
+    public void create(ReviewCreateRequest reviewCreateRequest, String ipAddress) {
+        reviewRepository.save(Review.from(ReviewCreate.from(reviewCreateRequest, ipAddress, passwordHolder), clockHolder));
+    }
+
     @Override
     public void updateRecommendations(Long id, Boolean recommendationFlag) {
         Review review = reviewRepository.get(id);
         review.updateRecommendations(recommendationFlag);
         reviewRepository.save(review);
+    }
+
+    @Override
+    public ReviewInfoResponse get(Long id) {
+        return ReviewInfoResponse.from(reviewRepository.get(id), clockHolder);
+    }
+
+    @Override
+    public PageResponse<ReviewResponse> getPagination(PageCreate pageCreate, ReviewSearch reviewSearch) {
+        PageResponse<Review> reviewPageResponse = reviewRepository.getPage(pageCreate, reviewSearch);
+        List<ReviewResponse> reviewResponseList = reviewPageResponse.getContent().stream()
+                .map(review -> ReviewResponse.from(review, clockHolder))
+                .toList();
+        return PageResponse.of(reviewResponseList, pageCreate, reviewPageResponse.getTotal());
+    }
+
+    @Override
+    public void delete(CommentableDelete commentableDelete, Long id) {
+        Review review = reviewRepository.get(id);
+        passwordHolder.match(commentableDelete.password(), review.getPassword());
+        reviewRepository.delete(review);
     }
 }
