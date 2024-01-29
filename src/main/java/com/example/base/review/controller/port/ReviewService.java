5eb7ac028e6c7900b64715955d01ddf70e1da0bd
akfrdma0125 <nogain1215@gmail.com>
2024-01-29 17:18:42 +0900

    feat(#15): 리뷰 상세 조회 기능 구현

--- src/main/java/com/example/base/review/controller/port/ReviewService.java
+++ src/main/java/com/example/base/review/controller/port/ReviewService.java
@@ -1,5 +1,20 @@
 package com.example.base.review.controller.port;
 
+import com.example.base.commentable.domain.dto.CommentableDelete;
+import com.example.base.review.controller.request.ReviewCreateRequest;
+import com.example.base.review.controller.response.ReviewInfoResponse;
+import com.example.base.review.controller.response.ReviewResponse;
+import com.example.base.review.domain.dto.ReviewSearch;
+import com.example.base.web.dto.PageCreate;
+import com.example.base.web.dto.PageResponse;
+
 public interface ReviewService {
+    public void create(ReviewCreateRequest reviewCreateRequest, String ipAddress);
     public void updateRecommendations(Long id, Boolean recommendationFlag);
+
+    ReviewInfoResponse get(Long id);
+
+    PageResponse<ReviewResponse> getPagination(PageCreate pageCreate, ReviewSearch reviewSearch);
+
+    void delete(CommentableDelete commentableDelete, Long id);
 }
