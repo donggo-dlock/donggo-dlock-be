f278b5f4dd74c6059c1d814c32ae4948ad272ef7
akfrdma0125 <nogain1215@gmail.com>
2024-01-29 17:16:55 +0900

    refactor(#15): 게시물 삭제 공통 처리

--- src/main/java/com/example/base/food/controller/port/FoodService.java
+++ src/main/java/com/example/base/food/controller/port/FoodService.java
@@ -3,7 +3,7 @@ package com.example.base.food.controller.port;
 import com.example.base.food.controller.request.FoodCreateRequest;
 import com.example.base.food.controller.response.FoodInfoResponse;
 import com.example.base.food.controller.response.FoodResponse;
-import com.example.base.food.domain.dto.FoodDelete;
+import com.example.base.commentable.domain.dto.CommentableDelete;
 import com.example.base.food.domain.dto.FoodSearch;
 import com.example.base.web.dto.PageCreate;
 import com.example.base.web.dto.PageResponse;
@@ -13,7 +13,7 @@ public interface FoodService {
     public PageResponse<FoodResponse> getPagination(PageCreate pageCreate, FoodSearch foodSearch);
     public FoodInfoResponse get(String name);
     public FoodInfoResponse get(Long id);
-    public void delete(FoodDelete foodDelete, Long id);
+    public void delete(CommentableDelete commentableDelete, Long id);
     public void updateRecommendations(Long id, Boolean recommendationFlag);
 
 }
