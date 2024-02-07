608c3e7dc2636a085276ba26cf9dc4f308fccdb8
akfrdma0125 <nogain1215@gmail.com>
2024-02-07 23:28:03 +0900

    refactor(#20): 음식 등록 불필요한 dto 및 정보 삭제

--- src/main/java/com/example/base/food/domain/Food.java
+++ src/main/java/com/example/base/food/domain/Food.java
@@ -3,7 +3,7 @@ package com.example.base.food.domain;
 import com.example.base.commentable.domain.Commentable;
 import com.example.base.common.service.port.ClockHolder;
 import com.example.base.common.service.port.PasswordHolder;
-import com.example.base.food.domain.dto.FoodCreate;
+import com.example.base.food.controller.request.FoodCreateRequest;
 import com.example.base.reportable.domain.ActiveStatus;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,15 +14,16 @@ public class Food extends Commentable {
     private int daysBeforeTest;
     private String mainIngredient;
 
-    public static Food from(FoodCreate foodCreate, ClockHolder clockHolder, PasswordHolder passwordHolder) {
+    public static Food from(FoodCreateRequest foodCreateRequest, String ipAddress, ClockHolder clockHolder, PasswordHolder passwordHolder) {
         Food food = new Food();
-        food.setName(foodCreate.name());
-        food.setContent(foodCreate.content());
-        food.setUserInformation(foodCreate.userInformation());
-        food.setPassword(passwordHolder.encrypt(foodCreate.password()));
+        String mainIngredient = String.join("&", foodCreateRequest.mainIngredient());
+        food.setName(foodCreateRequest.name());
+        food.setContent(foodCreateRequest.content());
+        food.setUserInformation(ipAddress);
+        food.setPassword(passwordHolder.encrypt(foodCreateRequest.password()));
         food.setStatus(ActiveStatus.ACTIVE);
-        food.setDaysBeforeTest(foodCreate.daysBeforeTest());
-        food.setMainIngredient(foodCreate.mainIngredient());
+        food.setDaysBeforeTest(foodCreateRequest.daysBeforeTest());
+        food.setMainIngredient(mainIngredient);
         food.setCreatedAt(clockHolder.millis());
         food.setLikes(0);
         food.setDislikes(0);
